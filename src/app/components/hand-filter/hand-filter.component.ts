3d054bef54a742525597091a57f34dbd1d1006aa
patrickfay <faypa01@gettysburg.edu>
2020-07-15 21:24:29 -0400

    set up basic functionality for hand-filter

--- src/app/components/hand-filter/hand-filter.component.ts
+++ src/app/components/hand-filter/hand-filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'hand-filter',
@@ -7,15 +8,47 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class HandFilterComponent implements OnInit {
 
+  // output event emitters
   @Output() drawCardEvent = new EventEmitter<boolean>();
   @Output() handFinishedEvent = new EventEmitter<boolean>();
   @Output() resetHandEvent = new EventEmitter<boolean>();
 
+  deckSize;       // TODO - replace this with actual deck size when get a deck obj
+  cardValOptions;
+
+  // form
+  filterFormGroup;
+  sizeOfHand;
+  selectedSuits;
+  maxCardVal;
+  minCardVal;
 
   constructor() { }
 
   ngOnInit() {
+    this.deckSize = 52;
+    this.cardValOptions = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+
+    this.filterFormGroup = new FormGroup({
+      sizeOfHand: new FormControl('', [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(this.deckSize)
+      ]),
+      diamondsSuit: new FormControl(true),
+      spadesSuit: new FormControl(true),
+      clubsSuit: new FormControl(true),
+      heartsSuit: new FormControl(true),
+      maxCardVal: new FormControl(this.cardValOptions[0]),
+      minCardVal: new FormControl(this.cardValOptions[0])
+    });
+
+    // // TODO - update value for max cards and any other vals needing update
+    // this.filterFormGroup.valueChanges.subscribe(newVal => {
+
+    // });
   }
+
 
   drawCard() {
     console.log('user drawing card');
@@ -28,8 +61,27 @@ export class HandFilterComponent implements OnInit {
   }
 
   userReset() {
-    console.log('user reset deck');
+    let _fgc = this.filterFormGroup.controls;
+
+    // reset form group vals
+    _fgc.sizeOfHand.setValue('');
+    _fgc.diamondsSuit.setValue(true);
+    _fgc.spadesSuit.setValue(true);
+    _fgc.clubsSuit.setValue(true);
+    _fgc.heartsSuit.setValue(true);
+    _fgc.maxCardVal.setValue(this.cardValOptions[0]);
+    _fgc.minCardVal.setValue(this.cardValOptions[0]);
+
+    // enable form group and emit reset val event
+    this.filterFormGroup.enable();
     this.resetHandEvent.emit();
+  }
+
+  submitForm() {
+    if (this.filterFormGroup.valid) {
+      this.filterFormGroup.disable();
+      this.drawCard();
+    }
   }
 
 }
